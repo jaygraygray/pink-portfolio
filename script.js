dcf7941b2fe73d162a1fa553976f198346045489
Jeremy Gray <jeremygray001@gmail.com>
2017-08-02 10:46:45 -0600

    Profile box finished.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function scrollToGenerateNums() {
+function profileMargins() {
   let scrollDistance = $(window).scrollTop()
   let margins = 120 - (scrollDistance / 2)
   if (margins > 28) {
@@ -9,19 +9,39 @@ function scrollToGenerateNums() {
 }
 
 function profileOpacity() {
-  let d = $(window).scrollTop()
-
+  return 1 - ($(window).scrollTop() * .007)
 }
+
+function reverseProfileOpacity() {
+  return ($(window).scrollTop() * .003)
+}
+
+function profileH1Margin() {
+ let d = $(window).scrollTop() / 2
+ if (d > 30) {
+   return d
+ } else {
+   return 30
+ }
+}
+
+
 
 $(document).ready(
   
   $(window).scroll(() => {
 
-    $('.box').css('margin-top', scrollToGenerateNums())
-    $('.box').css('margin-left', scrollToGenerateNums())
-    //$('.profile').css('opacity', 0)
-    $('.profile').css('top', scrollToGenerateNums())
-  })
+    $('.box').css('margin-top', profileMargins())
+    $('.box').css('margin-left', profileMargins())
 
+    $('.profile').css('opacity', profileOpacity())
+    $('.profile').css('top', profileMargins())
+
+    $('.box-title > h1').css('margin-right', profileH1Margin())
+
+    $('.profile-box-img').css('opacity', reverseProfileOpacity())
+  
+
+  })
 
 )
