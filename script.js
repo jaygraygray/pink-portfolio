fa755c9583295d2eda1babe597ceec1f80174c4d
Jeremy Gray <jeremygray001@gmail.com>
2017-08-06 16:45:29 -0600

    Project intro finished

--- script.js
+++ script.js
@@ -68,6 +68,15 @@ function contactPosition() {
 }
 
 
+function projectTransform(heightTrigger, stop, start) {
+  let d = $(window).scrollTop()
+  if (d > heightTrigger) {
+    return stop
+  } else {
+    return start
+  }
+}
+
 
 
 const box = $('.box')
@@ -78,7 +87,10 @@ const box = $('.box')
       projects = $('.projects')
       skills = $('.skills')
       contact = $('.contact')
-
+      housing = $('.housing')
+      medium = $('.medium')
+      sample = $('.sample')
+      snippets = $('.snippets')
 
 
 $(document).ready( function() {
@@ -115,31 +127,54 @@ $(document).ready( function() {
     contact.css('opacity', bioOpacity())
     contact.css('right', contactPosition())
 
+    // trigger skills display
     if (window.pageYOffset >= 490) {
-      // trigger skills display
+      
       console.log('triggs')
-        let kids = $('.skills-content').children()
+        $('.skills-content').children()
           .each( function(i) {
             delay = (i) * 100;
             setTimeout(function(div) {
-              div.show().addClass('icon-end')
+              div.addClass('icon-end')
             }, delay, $(this))
         })
     }
 
+    // trigger skills disappearance
     if (window.pageYOffset <= 600) {
-      // trigger skills display
-      console.log('triggs')
-        let kids = $('.skills-content').children()
-          .each( function(i) {
-            delay = (i) * 100;
-            setTimeout(function(div) {
-              div.show().removeClass('icon-end')
-              div.show().addClass('icon-start')
-            }, delay, $(this))
-        })
+      
+      $('.skills-content').children()
+        .each( function(i) {
+          let delay = (i) * 100;
+          setTimeout(function(div) {
+            div.removeClass('icon-end')
+            div.addClass('icon-start')
+          }, delay, $(this))
+      })
     }
-  
+    console.log($(window).scrollTop())
+    // display projects
+    housing.css({
+      'top' : projectTransform(1178, 60, 183),
+      'right' : projectTransform(1178, 48, 181),
+      'width' : projectTransform(1178, 657, 400)
+    })                              
+     medium.css({
+       'top' : projectTransform(1268, 189, 312),
+       'right' : projectTransform(1268, 48, 181),
+       'width' : projectTransform(1268, 657, 400)
+     })                    
+    sample.css( {
+      'top': projectTransform(1358, 318, 441),
+      'right': projectTransform(1358, 48, 181),
+      'width' : projectTransform(1358, 657, 400)
+    })                    
+    snippets.css( {
+      'top': projectTransform(1458, 447, 570),
+      'right': projectTransform(1458, 48, 181),
+      'width' : projectTransform(1458, 657, 400)
+    }) 
+    
   })
 
 })
